56085539b247238a69ee591f8d6e55fb8c4c7da7
Mateus Ramos Pereira <mateus.pereira@pr.senac.br>
2025-08-15 12:34:31 -0300

    chore: remover dependência Node e desativar verbatimModuleSyntax no tsconfig

--- src/01-sequential/terreno.ts
+++ src/01-sequential/terreno.ts
@@ -1,9 +1,9 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
-const largura = Number(prompt("Digite a largura do terreno:"));
-const comprimento = Number(prompt("Digite o comprimento do terreno:"));
-const valorM2 = Number(prompt("Digite o valor do metro quadrado:"));
+const largura = Number(prompt("Digite a largura do terreno: "));
+const comprimento = Number(prompt("Digite o comprimento do terreno: "));
+const valorM2 = Number(prompt("Digite o valor do metro quadrado: "));
 
 const area = largura * comprimento;
 const preco = area * valorM2;
